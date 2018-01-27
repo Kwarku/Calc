4fd26f389e727992429dd448245411becf307545
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-01-27 14:48:27 +0100

    conflict test

---
+++ src/main/java/Week5/Calculator.java
@@ -0,0 +1,11 @@
+package Week5;
+
+public class Calculator implements Operations {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double minus(double a, double b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/Week5/Operations.java
@@ -0,0 +1,8 @@
+package Week5;
+
+public interface Operations {
+    double add(double a, double b);
+
+    double minus(double a, double b);
+
+}
