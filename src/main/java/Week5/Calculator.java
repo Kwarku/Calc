32812c38e99b9c06535fa36cacc521f5ef9d51d5
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-01-27 14:22:58 +0100

    add task 1

---
+++ src/main/java/Week5/Calculator.java
@@ -0,0 +1,11 @@
+package Week5;
+
+public class Calculator implements Operations {
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int minus(int a, int b) {
+        return a-b;
+    }
+}
